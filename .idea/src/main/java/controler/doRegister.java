1c18f43111bc8b915fa2d81f608af0f3a8d01a03
nhson2612 <nhson26122004@gmail.com>
2023-02-19 01:50:36 +0700

    1:50 2/19/2023

---
+++ .idea/src/main/java/controler/doRegister.java
@@ -0,0 +1,57 @@
+package controler;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import model.Customer;
+import model.DAOCustomer;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Date;
+
+public class doRegister extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        try {
+
+            String name = request.getParameter("name");
+            String numberPhone = request.getParameter("numberPhone");
+            String dateOfBirth = request.getParameter("dateOfBirth");
+            String sSex = request.getParameter("sex");
+            boolean sex = (sSex.equals("Nam")) ? true : false;
+            String address = request.getParameter("address");
+            String email = request.getParameter("email");
+            String passWord = request.getParameter("passWord");
+            String confirmPassWord = request.getParameter("confirmPassWord");
+            String error = request.getParameter("error");
+
+            if (!passWord.equals(confirmPassWord)) { // xác nhận mật khẩu không khớp
+                request.setAttribute("error", "xác nhận mật khẩu không khớp");
+                RequestDispatcher rd = request.getRequestDispatcher("/register.jsp");
+                rd.forward(request, response);
+                System.out.println("xac nhan mat khau khong khop");
+            } else if (DAOCustomer.getInstance().seachUser(name)) { // username đã tồn tại
+                request.setAttribute("error", "username đã tồn tại");
+                RequestDispatcher rd = request.getRequestDispatcher("/register.jsp");
+                rd.forward(request, response);
+                System.out.println("user name da ton tai");
+            } else if (DAOCustomer.getInstance().searchEmail(email)) { // email đã tồn tại
+                request.setAttribute("error", "email đã được đăng kí");
+                RequestDispatcher rd = request.getRequestDispatcher("/register.jsp");
+                rd.forward(request,response);
+                System.out.println("Email da duoc dang ki");
+            }else {
+                Customer customer = new Customer(name,Long.valueOf(numberPhone), Date.valueOf(dateOfBirth),sex,address,email,passWord);
+                DAOCustomer.getInstance().insert(customer);
+                RequestDispatcher rd = request.getRequestDispatcher("/success.jsp");
+                rd.forward(request,response);
+                System.out.println("khong van de j");
+            }
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+}
